af6e232443f9ceafd7059fcdb398b6dc41e1ef55
Fetiya-Ahmed <fetiyaebrahim74@gmail.com>
2024-02-03 22:02:27 -0600

    netflix project

---
+++ src/utils/requests.js
@@ -0,0 +1,15 @@
+const API_KEY = "fb93af005b98559a2b5ea2f73b4ba585"
+
+const requests ={
+  fetchTrending: `/trending/all/day?api_key=${API_KEY}`,
+  fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
+  fetchTopRatedMovies: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
+  fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
+  fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with_genres=35`,
+  fetchHorrorMovies: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
+  fetchRomanceMovies: `/discover/movie?api_key=${API_KEY}&with_genres=10749`,
+  fetchDocumentaries: `/discover/movie?api_key=${API_KEY}&with_genres=99`,
+  fetchTvShows: `/tv/popular?api_key=${API_KEY}&language=en-US&page=1`
+};
+
+export default requests;
